5df2670a056c25ea18067821bec99af239ec07e3
Emily O <opresni02@gmail.com>
2023-04-11 12:08:32 -0400

    changed location api, added info to get access token and started method to generate playlist, username still in works

--- src/app.py
+++ src/app.py
@@ -2,10 +2,12 @@ from flask import Flask, request, render_template, jsonify,url_for,redirect,sess
 import webbrowser
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+from spotipy import Spotify
 import os, requests
 import json
 
 app = Flask(__name__)
+
 
 
 # OAuth
@@ -16,6 +18,14 @@ weatherApi = secrets["openWeather"]
 locationApi = secrets["location"]
 
 
+#for spotify 
+BASE_URL = 'https://api.spotify.com/v1/'
+client_id=secrets["client_id"]
+client_secret =secrets['client_secret']
+
+
+
+
 # Default page
 @app.route("/")
 def hello():
@@ -23,9 +33,10 @@ def hello():
 
 @app.route("/login")
 def SpotifyLogin():
-      sp_oauth = create_spotify_oauth()
-      auth_url = sp_oauth.get_authorize_url()
-      return redirect(auth_url)
+    sp_oauth = create_spotify_oauth()
+    auth_url = sp_oauth.get_authorize_url()
+    return redirect(auth_url)
+
 
 @app.route("/logout")
 def SpotifyLogout():
@@ -33,12 +44,16 @@ def SpotifyLogout():
       return render_template('home.html', message="You have been logged out.")
 
 
+
+
 def create_spotify_oauth():
     return SpotifyOAuth(
             client_id=secrets["client_id"],
             client_secret=secrets["client_secret"],
         redirect_uri="http://127.0.0.1:5000/redirect/",
-         scope="playlist-read-private")
+         scope=["user-read-private", "user-read-email", 'playlist-modify-public', 
+                'playlist-modify-private', 'playlist-read-private', 'user-library-modify', 
+                'user-library-read', 'user-top-read'])
 
 
 def weatherSearch(name):
@@ -48,19 +63,84 @@ def weatherSearch(name):
 
 
 def getLocation():
-    URL = 'http://api.ipstack.com/check?access_key='+locationApi
-    geo_req = requests.get(URL)
-    geo_json = json.loads(geo_req.text)
-    latitude = geo_json['latitude']
-    longitude = geo_json['longitude']
-    city = geo_json['city']
-    return city
+    URL = "https://api.ipdata.co?api-key="+locationApi
+    response = requests.get(URL)
+    geo=response.json()
+    return geo['city'] 
+
+
+
+def getUserName():
+    headers = {'Authorization': 'Bearer {token}'.format(token=session.get('acess_token'))}
+    r = requests.get(BASE_URL + 'me', headers=headers)
+    r=r.json()
+    return 'user' #r['display_name']
+
+def getLikedSongs():
+    headers = {'Authorization': 'Bearer {token}'.format(token=session.get('acess_token'))}
+    r= requests.get(BASE_URL + "me/tracks" , headers=headers)
+    r=r.json()
+    return 'need to finish'
+    #returns file containing (default 20) of the users liked songs
+
+def userTopArtistSeeds():
+    headers = {'Authorization': 'Bearer {token}'.format(token=session.get('acess_token'))}
+    limit =5
+    timeRange ="medium_term"
+    r= requests.get(BASE_URL + "me/top/artists?limit=" +limit + "&time_range="+timeRange, headers=headers)
+    r=r.json()
+    return "todo"
+
+
+def userTopGenreSeeds():
+    return "todo"
+    
+
+def userTopTracksSeeds():
+     #need to get the top genres of a user to curate recs
+    return "todo"
+
+def getRecsClear():
+     headers = {'Authorization': 'Bearer {token}'.format(token=session.get('acess_token'))}
+     r=requests.get(BASE_URL + "recommendations", headers=headers)
+     return "need to finish"
+
+
+def getRecsRain():
+     return 'TODO'
+     
+def getRecsDrizzle():
+     #shower rain
+     return 'TODO'
+
+def getRecsThunder():
+     return 'TODO'
+     
+def getRecsSnow():
+     return 'TODO'
+
+def getRecsClouds():
+     #grouped together few clouds, scattered clouds, broken clouds
+     return 'TODO'
+
+def getRecsMist():
+     return 'TODO'
+     
+ 
+
+
 
 @app.route("/redirect/")
 def redirectPage():
-      cityName = getLocation()
-      result = weatherSearch(cityName)
-      return render_template('redirect.html', name='Tony', weatherResponse=True, cityName=result.get("name"), temp=result.get("main").get("temp"), description=result.get("weather")[0].get("description"), h=result.get("main").get("humidity"))
+    sp_oauth = create_spotify_oauth()
+    code = request.args.get('code')
+    session.clear()
+    code = request.args.get('code')
+    token_info = sp_oauth.get_access_token(code)
+    session['token_info'] = token_info
+    cityName = getLocation()
+    result = weatherSearch(cityName)
+    return render_template('redirect.html', name=getUserName() , weatherResponse=True, cityName=result.get("name"), temp=result.get("main").get("temp"), description=result.get("weather")[0].get("description"), h=result.get("main").get("humidity"))
 
 
 if __name__ == "__main__":
